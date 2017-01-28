a6c898172f2233c33b774fb1df36aecb1a25453e
amarmohanty <amohanty@vt.edu>
2017-01-27 23:09:03 -0500

    Aye

--- Fuck/src/Shit.java
+++ Fuck/src/Shit.java
@@ -3,11 +3,16 @@ public class Shit {
 
     public static void main(String[] args) {
         // TODO Auto-generated m
-        System.out.println("Hello");
+        System.out.println("You are a hoe");
+        fuck();
+        myfavoriteNumb();
     }
-    public void fuck()
+    public static void fuck()
     {
        System.out.println("ohhh yahhh");
     }
-
+    public static int myfavoriteNumb()
+    {
+     return 7;
+    }
 }

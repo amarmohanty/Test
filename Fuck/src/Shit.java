e4c665664eb253c778d206170d3e37e5c242be3d
forteddyt <forteddyt@gmail.com>
2017-01-27 23:08:50 -0500

    Teddy's commit

--- Fuck/src/Shit.java
+++ Fuck/src/Shit.java
@@ -1,13 +1,22 @@
+import java.util.ArrayList;
 
 public class Shit {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated m
-        System.out.println("Hello");
-    }
-    public void fuck()
-    {
-       System.out.println("ohhh yahhh");
-    }
+	public static void main(String[] args) {
+		// TODO Auto-generated m
+		System.out.println("WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOSHIT!");
+	}
+
+	public void fuck() {
+		System.out.println("ohhh yahhh");
+	}
+
+	public void someMethod() {
+		System.out.println("This is some method that I wrote to create a merge conflict! Hopefully this works!");
+
+		System.out.println("MOAR CODE!");
+
+		ArrayList<String> temp = new ArrayList<String>();
+	}
 
 }
